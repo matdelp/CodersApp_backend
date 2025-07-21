f93649b6da9102737795e8db766a6db3106074ac
matdelp <mathilde.delpeuch@gmail.com>
2025-07-21 18:24:23 +0200

    feat: heatmap endpoint added

--- src/controllers/heatmapController.ts
+++ src/controllers/heatmapController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { heatmap } from "../data"; //fetch from db later
+import mongoose from "mongoose";
+import { CoderModel } from "../models/Coder";
 
 export const heatmapController = {
   getHeatmap: async (req: Request, res: Response) => {
@@ -25,6 +27,59 @@ export const heatmapController = {
       } else {
         setEndDate = new Date(new Date());
       }
-    } catch (error: any) {}
+
+      const coderId = new mongoose.Types.ObjectId(userId as string);
+
+      const submitDates = await CoderModel.aggregate([
+        { $match: { _id: coderId } },
+        { $unwind: "$submission" },
+        {
+          $lookup: {
+            from: "submission",
+            localField: "submission",
+            foreignField: "_id",
+            as: "submissionList",
+          },
+        },
+        { $unwind: "$submissionList" },
+        {
+          $match: {
+            "submissionList.status": "passed",
+            "submissionList.createdAt": {
+              $gte: setStartDate,
+              $lte: setEndDate,
+            },
+          },
+        },
+        {
+          $addFields: {
+            date: {
+              $dateToString: {
+                format: "%Y/%m/%d",
+                date: "$submissionList.createdAt",
+              },
+            },
+          },
+        },
+        {
+          $group: {
+            _id: "$date",
+            count: { $sum: 1 },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+            date: "$_id",
+            count: 1,
+          },
+        },
+        { $sort: { date: 1 } },
+      ]);
+
+      res.status(200).json({ submitDates });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
   },
 };
